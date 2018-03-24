af30662c7773bef750624ba581cb33a772d4f6e6
stefanstojanov <stefan_stojanov@live.com>
2018-03-24 12:01:30 +0100

    edit rezultat
    gotovo

--- resources/views/results/edit.blade.php
+++ resources/views/results/edit.blade.php
@@ -1,16 +1,27 @@
 @extends('layouts.app')
+@include('layouts.nav')
     @section('content')
-        <form method="post" action="/edit_result">
+<center>
+<div class="card" style="border:1px solid #00a4a2; margin-top:100px; width:40%">
+    <div class="card-header" style="width:100%; background-color:00a4a2; color:white;"> Измена на веќе постоечки резултат </div>
+    <div class="card-body">    
+    <form method="post" action="/edit_result">
             {{csrf_field()}}
             <?php $i=1; ?>
         @foreach($stavki as $stavka)
-                <h3>Ime</h3><h3>{{$stavka->name}}</h3><br>
-                <input type="hidden" value="{{$result->id}}" name="result_id">
-                <h3>Vrednost</h3><input type="text" name="value{{$i}}" value="{{$stavka->value}}"><br>
-                <input type="hidden" value="{{$stavka->item_id}}" name="item_id{{$i}}">
+            <div class="row">
+                <div class="col">
+            {{$stavka->name}}
+                <input type="hidden" value="{{$result->id}}" name="result_id" class="form-group">
+                <input type="text" name="value{{$i}}" value="{{$stavka->value}}" class="form-group">
+                <input type="hidden" value="{{$stavka->item_id}}" name="item_id{{$i}}" class="form-group">
             <?php $i++ ?>
+                </div>
+                </div>
         @endforeach
-            <button type="submit">Ajde</button>
+            <button type="button" class="btn" style="background-color:00a4a2; color:white;">Ajde</button>
         </form>
-
+    </div>
+</div>
+    </center>
     @endsection
